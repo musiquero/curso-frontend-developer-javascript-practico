7b386ecdfb14013d3b823e5d9b64a5465d0fb8d5
musiqero <ylichorsini@gmail.com>
2023-03-14 17:13:03 -0400

    Ya he agregado el menú interactivo en javascript

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerIcon = document.querySelector(".menu");
+const carritoIcon = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerIcon.addEventListener("click", toggelMenuMobile);
+carritoIcon.addEventListener("click", toggelCarritoAside)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggelMenuMobile() {
+    aside.classList.add("inactive");
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggelCarritoAside() {
+    mobileMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");
 }
